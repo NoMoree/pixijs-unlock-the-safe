6ba46c24270633425649c80d0ecc2013b2835002
NoMoree <i9q19i@gmail.com>
2026-04-24 11:52:35 +0300

    add temp assets

---
+++ src/structure/blocks/setup/AssetLoad.ts
@@ -0,0 +1,28 @@
+import { Assets } from "pixi.js";
+import { Block } from "../../Blocks";
+
+export class AssetLoad extends Block {
+
+    constructor(name: string) {
+        super(name);
+    }
+
+    start() {
+        this._loadAssets().then(() => {
+            this.end();
+        });
+    }
+
+    private async _loadAssets(): Promise<void> {
+        const baseUrl = '/assets/images/';
+        Assets.addBundle('all-assets', {
+            'background': `${baseUrl}background.png`,
+            'door-closed': `${baseUrl}door-closed.png`,
+            'door-open': `${baseUrl}door-open.png`,
+            'door-handle': `${baseUrl}door-handle.png`,
+            'treasure': `${baseUrl}treasure.png`,
+        });
+
+        await Assets.loadBundle('all-assets');
+    }
+}
